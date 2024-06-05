1e73d71d83d3e2ac1cd95ff72dd756756a1a1416
yasirupanduka99 <22panduka@gmail.com>
2024-06-05 23:24:54 +0530

    Adding about JSON into HTML file ✨

---
+++ 05_JS_Asynchronous - JSON Data/script.js
@@ -0,0 +1,32 @@
+
+const getTodos = (callback) => {
+  const request = new XMLHttpRequest();
+
+  request.addEventListener("readystatechange", () => {
+    if (request.readyState === 4 && request.status === 200) {
+      const jsonData = JSON.parse(request.responseText); // This JSON.parse() method do what is JSON data is strings. so this method convert(parse) those string to javascript objects.
+      callback(undefined, jsonData);
+    } else if (request.readyState === 4) {
+      callback("could not fetch the data!", undefined);
+    }
+  });
+
+  request.open("GET", "https://jsonplaceholder.typicode.com/todos/");
+  request.send();
+};
+
+
+console.log(1);
+console.log(2);
+
+getTodos((err, data) => {
+  console.log("Callback fired!");
+  if (err) {
+    console.log(err);
+  } else {
+    console.log(data);
+  }
+});
+
+console.log(3);
+console.log(4);
